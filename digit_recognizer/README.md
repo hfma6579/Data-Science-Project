74408f1f1eb422f058ef51683659f0e3a517a33d
mike <zhtmike@gmail.com>
2018-03-13 14:54:30 +0800

    init project of digit recognizer

---
+++ digit_recognizer/README.md
@@ -0,0 +1,3 @@
+# Digit Recognizer
+
+<https://www.kaggle.com/c/digit-recognizer>
---
+++ digit_recognizer/run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+An machine learning exercise with TensorFlow
+"""
+def parse(src):
+    raise NotImplementedError
+
+def train(X, y):
+    raise NotImplementedError
+
+def main():
+    TRAIN = 'data/train.csv'
+    TEST = 'data/test.csv'
+
+
+
+if __name__ == '__main__':
+    main()
