f345808270307d9594551a3a883a065fc7e4d361
mike <zhtmike@gmail.com>
2018-03-16 22:35:52 +0800

    using early stopping

--- digit_recognizer/run.py
+++ digit_recognizer/run.py
@@ -1,20 +1,41 @@
-#!/usr/bin/env python3
-"""
-An machine learning exercise with TensorFlow
-"""
-
 import tensorflow as tf
 import numpy as np
-import pandas as pd
 
 from sklearn.model_selection import train_test_split
 
-tf.logging.set_verbosity(tf.logging.INFO)
+
+NUM_EPOCHS = 5           # Patience in early stopping
+BATCH_SIZE = 64          # Batch size
 
 
-def cnn_model_fn(features, labels, mode):
+def train_input_fn(features, labels, batch_size):
+    # iterator for training data
+    dataset = tf.data.Dataset.from_tensor_slices(
+        ({'pixel': features}, labels))
+    dataset = dataset.shuffle(10000).repeat().batch(batch_size)
+    return dataset
+
+
+def eval_input_fn(features, labels, batch_size):
+    # iterator for evaluation data
+    dataset = tf.data.Dataset.from_tensor_slices(
+        ({'pixel': features}, labels))
+    dataset = dataset.batch(batch_size)
+    return dataset
+
+
+def predict_input_fn(features, batch_size):
+    # iterator for prediction data
+    dataset = tf.data.Dataset.from_tensor_slices(
+        {'pixel': features})
+    dataset = dataset.batch(batch_size)
+    return dataset
+
+
+def cnn_model_fn(features, labels, mode, params):
     # input layer
-    input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+    input_layer = tf.reshape(tf.feature_column.input_layer(
+        features, params['feature_columns']), [-1, 28, 28, 1])
 
     # Convolutional Layer #1
     conv1 = tf.layers.conv2d(
@@ -67,98 +88,88 @@ def cnn_model_fn(features, labels, mode):
     # Logits Layer
     logits = tf.layers.dense(inputs=dense, units=10)
 
-    # And below are copied from tutorial
-    predictions = {
-        # Generate predictions (for PREDICT and EVAL mode)
-        "classes": tf.argmax(input=logits, axis=1),
-        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-        # `logging_hook`.
-        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-    }
-
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
-
-    # Calculate Loss (for both TRAIN and EVAL modes)
     loss = tf.losses.sparse_softmax_cross_entropy(
         labels=labels,
         logits=logits
     )
 
-    # Configure the Training Op (for TRAIN mode)
     if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        optimizer = tf.train.MomentumOptimizer(
+            learning_rate=0.01,
+            momentum=0.9)
+
         train_op = optimizer.minimize(
             loss=loss,
             global_step=tf.train.get_global_step()
         )
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
-    # Add evaluation metrics (for EVAL mode)
-    eval_metric_ops = {
-        "accuracy": tf.metrics.accuracy(
-            labels=labels,
-            predictions=predictions["classes"]
-        )
-    }
-    return tf.estimator.EstimatorSpec(
-        mode=mode,
-        loss=loss,
-        eval_metric_ops=eval_metric_ops
-    )
+    if mode == tf.estimator.ModeKeys.EVAL:
+        eval_metric_ops = {
+            "accuracy": tf.metrics.accuracy(
+                labels=labels,
+                predictions=tf.argmax(input=logits, axis=1)
+            )
+        }
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+    else:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions={"classes": tf.argmax(input=logits, axis=1)})
 
 
 def main(argv):
     # Load training data
-    data = np.load('data/train_X.npy').astype(np.float32)
-    labels = np.load('data/train_y.npy').astype(np.int32)
-    train_data, eval_data, train_labels, eval_labels = train_test_split(
-        data, labels, random_state=0)
+    data = np.load('data/train_test.npz')
+    train_x, eval_x, train_y, eval_y = train_test_split(
+        data['x'], data['y'].astype(np.int32), random_state=0)
 
-    classifier = tf.estimator.Estimator(cnn_model_fn)
+    # Create feature columns, here there is only one feature (* 28 * 28).
+    feature_columns = [
+        tf.feature_column.numeric_column(
+            key="pixel",
+            shape=[28, 28],
+            normalizer_fn=lambda x: tf.image.per_image_standardization(x))]
 
-    tensors_to_log = {"probabilities": "softmax_tensor"}
-    logging_hook = tf.train.LoggingTensorHook(
-        tensors=tensors_to_log,
-        every_n_iter=50
-    )
+    # create classifier
+    classifier = tf.estimator.Estimator(
+        model_fn=cnn_model_fn,
+        params={
+            'feature_columns': feature_columns
+        })
 
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": train_data},
-        y=train_labels,
-        batch_size=100,
-        num_epochs=None,
-        shuffle=True
-    )
+    # set up a logger
+    # logging_hook = tf.train.LoggingTensorHook(
+    #     tensors={'learning rate': 'learning_rate'},
+    #     every_n_iter=10)
 
-    classifier.train(
-        input_fn=train_input_fn,
-        steps=5000,
-        hooks=[logging_hook]
-    )
+    # performs a early stopping for each $NUM_EPOCHS epoches
+    steps_per_train = train_y.size * NUM_EPOCHS // BATCH_SIZE
+    old_eval_result = 0
+    counter = 0
 
-    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": eval_data},
-        y=eval_labels,
-        num_epochs=1,
-        shuffle=False
-    )
+    while(counter < 2):
+        counter += 1
+        classifier.train(
+            input_fn=lambda: train_input_fn(train_x, train_y, BATCH_SIZE),
+            steps=steps_per_train)
 
-    eval_results = classifier.evaluate(input_fn=eval_input_fn)
-    print(eval_results)
+        new_eval_result = classifier.evaluate(
+            input_fn=lambda: eval_input_fn(eval_x, eval_y, BATCH_SIZE))['accuracy']
 
-    test_data = np.load('data/test_X.npy').astype(np.float32)
-    test_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": test_data},
-        num_epochs=1,
-        shuffle=False
-    )
+        print("Accuracy: {}".format(new_eval_result))
+        # if there is a improve on the evaluation set, set the counter to 0
+        if new_eval_result > old_eval_result:
+            counter = 0
 
-    predictions = classifier.predict(test_input_fn)
+    test_x = np.load('data/test.npz')
+    predictions = classifier.predict(
+        input_fn=lambda: predict_input_fn(test_x, BATCH_SIZE))
     labels = [pred['classes'] for pred in predictions]
-    df = pd.DataFrame({'ImageId': range(1, len(labels)+1), 'Label': labels})
+    df = pd.DataFrame({'ImageId': range(1, len(labels) + 1), 'Label': labels})
     df.set_index('ImageId', inplace=True)
     df.to_csv('submission.csv')
 
+
 if __name__ == '__main__':
-    tf.app.run()
+    # tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run(main)
