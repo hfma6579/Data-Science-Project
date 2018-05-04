35d7a9157232ad91087f0ceb15b14d93dc954c47
mike <zhtmike@gmail.com>
2018-05-04 22:47:32 +0800

    Improve algorithm
    - Add dropout
    - Add L2 regularization
    - Change to AdamOptimizer

--- digit_recognizer/run.py
+++ digit_recognizer/run.py
@@ -1,3 +1,7 @@
+"""
+CNN with 4 convolution layer and 1 dense layer
+Achieve 0.99228 in MNIST data recognition
+"""
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -7,13 +11,14 @@ from sklearn.model_selection import train_test_split
 
 NUM_EPOCHS = 5           # Patience in early stopping
 BATCH_SIZE = 64          # Batch size
+CROSS_EVAL = True        # Mode for cross evaluation
 
 
 def train_input_fn(features, labels, batch_size):
     # iterator for training data
     dataset = tf.data.Dataset.from_tensor_slices(
         ({'pixel': features}, labels))
-    dataset = dataset.shuffle(10000).repeat().batch(batch_size)
+    dataset = dataset.shuffle(BATCH_SIZE).repeat().batch(batch_size)
     return dataset
 
 
@@ -37,6 +42,9 @@ def cnn_model_fn(features, labels, mode, params):
     # input layer
     input_layer = tf.reshape(tf.feature_column.input_layer(
         features, params['feature_columns']), [-1, 28, 28, 1])
+    input_layer = tf.layers.dropout(inputs=input_layer, rate=0.1)
+
+    regularizer = tf.contrib.layers.l2_regularizer(0.1, scope=None)
 
     # Convolutional Layer #1
     conv1 = tf.layers.conv2d(
@@ -45,7 +53,8 @@ def cnn_model_fn(features, labels, mode, params):
         filters=32,
         kernel_size=[3, 3],
         padding="same",
-        activation=tf.nn.relu
+        activation=tf.nn.relu,
+        kernel_regularizer=regularizer
     )
 
     # Convolutional Layer #2
@@ -55,7 +64,8 @@ def cnn_model_fn(features, labels, mode, params):
         filters=32,
         kernel_size=[5, 5],
         padding="same",
-        activation=tf.nn.relu
+        activation=tf.nn.relu,
+        kernel_regularizer=regularizer
     )
 
     # Convolutional Layer #3
@@ -65,7 +75,8 @@ def cnn_model_fn(features, labels, mode, params):
         filters=64,
         kernel_size=[3, 3],
         padding="same",
-        activation=tf.nn.relu
+        activation=tf.nn.relu,
+        kernel_regularizer=regularizer
     )
 
     # Convolutional Layer #4
@@ -75,7 +86,8 @@ def cnn_model_fn(features, labels, mode, params):
         filters=64,
         kernel_size=[5, 5],
         padding="same",
-        activation=tf.nn.relu
+        activation=tf.nn.relu,
+        kernel_regularizer=regularizer
     )
 
     # Dense Layer
@@ -83,11 +95,14 @@ def cnn_model_fn(features, labels, mode, params):
     dense = tf.layers.dense(
         inputs=conv4_flat,
         units=1024,
-        activation=tf.nn.relu
+        activation=tf.nn.relu,
+        kernel_regularizer=regularizer
     )
+    dense = tf.layers.dropout(inputs=dense)
 
     # Logits Layer
-    logits = tf.layers.dense(inputs=dense, units=10)
+    logits = tf.layers.dense(inputs=dense, units=10,
+                             kernel_regularizer=regularizer)
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions={"classes": tf.argmax(input=logits, axis=1)})
@@ -98,9 +113,9 @@ def cnn_model_fn(features, labels, mode, params):
     )
 
     if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.MomentumOptimizer(
-            learning_rate=0.01,
-            momentum=0.9)
+        optimizer = tf.train.AdamOptimizer(
+            learning_rate=0.001,
+        )
 
         train_op = optimizer.minimize(
             loss=loss,
@@ -121,8 +136,6 @@ def cnn_model_fn(features, labels, mode, params):
 def main(argv):
     # Load training data
     data = np.load('data/train.npz')
-    train_x, eval_x, train_y, eval_y = train_test_split(
-        data['x'], data['y'].astype(np.int32), random_state=0)
 
     # Create feature columns, here there is only one feature (* 28 * 28).
     feature_columns = [
@@ -138,32 +151,37 @@ def main(argv):
             'feature_columns': feature_columns
         })
 
-    # set up a logger
-    # logging_hook = tf.train.LoggingTensorHook(
-    #     tensors={'learning rate': 'learning_rate'},
-    #     every_n_iter=10)
+    if CROSS_EVAL:
+        train_x, eval_x, train_y, eval_y = train_test_split(
+            data['x'], data['y'].astype(np.int32), test_size=0.2, random_state=0)
 
-    # performs a early stopping for each $NUM_EPOCHS epoches
-    steps_per_train = train_y.size * NUM_EPOCHS // BATCH_SIZE
-    old_eval_result = 0
-    counter = 0
+        # performs a early stopping for each $NUM_EPOCHS epoches
+        steps_per_train = train_y.size * NUM_EPOCHS // BATCH_SIZE
+        old_eval_result = 0
+        counter = 0
 
-    while(counter < 2):
-        counter += 1
-        classifier.train(
-            input_fn=lambda: train_input_fn(train_x, train_y, BATCH_SIZE),
-            steps=steps_per_train)
+        while(counter < 3):
+            counter += 1
+            classifier.train(
+                input_fn=lambda: train_input_fn(train_x, train_y, BATCH_SIZE),
+                steps=steps_per_train)
 
-        new_eval_result = classifier.evaluate(
-            input_fn=lambda: eval_input_fn(eval_x, eval_y, BATCH_SIZE))['accuracy']
+            new_eval_result = classifier.evaluate(
+                input_fn=lambda: eval_input_fn(eval_x, eval_y, BATCH_SIZE))['accuracy']
 
-        print("Accuracy: {}".format(new_eval_result))
-        # if there is a improve on the evaluation set, set the counter to 0
-        if new_eval_result - old_eval_result > 1E-5:
-            counter = 0
-        old_eval_result = new_eval_result
-        print("Current counter: {}".format(counter))
+            print("Accuracy: {}".format(new_eval_result))
+            # if there is a improve on the evaluation set, set the counter to 0
+            if new_eval_result - old_eval_result > 1E-5:
+                counter = 0
+            old_eval_result = new_eval_result
+            print("Current counter: {}".format(counter))
+            return
 
+    full_x, full_y = data['x'], data['y'].astype(np.int32)
+    classifier.train(
+        input_fn=lambda: train_input_fn(full_x, full_y, BATCH_SIZE),
+        steps=30000
+    )
     test_x = np.load('data/test.npz')['x']
     predictions = classifier.predict(
         input_fn=lambda: predict_input_fn(test_x, BATCH_SIZE))
@@ -174,5 +192,5 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    # tf.logging.set_verbosity(tf.logging.INFO)
+    tf.logging.set_verbosity(tf.logging.INFO)
     tf.app.run(main)
