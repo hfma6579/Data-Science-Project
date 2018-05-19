ad43522867e94ac69537bcebe40e668e55336739
mike <zhtmike@gmail.com>
2018-05-19 21:32:28 +0800

    update code and readme

--- digit_recognizer/README.md
+++ digit_recognizer/README.md
@@ -1,3 +1,12 @@
 # Digit Recognizer
 
+CNN with 2 Convolution layers for MNIST Data set
+
 <https://www.kaggle.com/c/digit-recognizer>
+
+## Usage
+
+1. For training, run `python run.py`;
+2. For prediction, run `python run --predict;`
+3. Run `python run.py --h` for other options;
+4. Run `tensorboard --logdir=./tmp/mnist_cnn/` for visualization
--- digit_recognizer/run.py
+++ digit_recognizer/run.py
@@ -1,24 +1,30 @@
 """
-CNN with 4 convolution layer and 1 dense layer
-Achieve 0.99228 in MNIST data recognition
+CNN with 2 Convolution layers for MNIST Data set
+
 """
+import os
+
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
+# flag settings
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string('model_dir', './tmp/mnist_cnn', """Path to the model directory.""")
+tf.app.flags.DEFINE_string('data_dir', './data/', """Path to the data directory.""")
+tf.app.flags.DEFINE_bool('heldout', False, """Perform cross evluation.""")
+tf.app.flags.DEFINE_bool('predict', False, """Perform prediction.""")
+tf.app.flags.DEFINE_integer('max_iter', 10000, """Maximum number of iteration""")
 
-
-NUM_EPOCHS = 5           # Patience in early stopping
-BATCH_SIZE = 64          # Batch size
-CROSS_EVAL = True        # Mode for cross evaluation
+BATCH_SIZE = 64
+EVAL_BATCH_SIZE = 1024
 
 
 def train_input_fn(features, labels, batch_size):
     # iterator for training data
     dataset = tf.data.Dataset.from_tensor_slices(
         ({'pixel': features}, labels))
-    dataset = dataset.shuffle(BATCH_SIZE).repeat().batch(batch_size)
+    dataset = dataset.shuffle(batch_size).repeat().batch(batch_size)
     return dataset
 
 
@@ -40,69 +46,48 @@ def predict_input_fn(features, batch_size):
 
 def cnn_model_fn(features, labels, mode, params):
     # input layer
-    input_layer = tf.reshape(tf.feature_column.input_layer(
-        features, params['feature_columns']), [-1, 28, 28, 1])
-    input_layer = tf.layers.dropout(inputs=input_layer, rate=0.1)
-
-    regularizer = tf.contrib.layers.l2_regularizer(0.1, scope=None)
+    input_layer = tf.feature_column.input_layer(features, params['feature_columns'])
+    input_layer = tf.reshape(input_layer, [-1, 28, 28, 1])
+    tf.summary.image('input', input_layer, 1)
 
     # Convolutional Layer #1
     conv1 = tf.layers.conv2d(
         inputs=input_layer,
-        strides=(1, 1),
+        strides=(2, 2),
         filters=32,
-        kernel_size=[3, 3],
+        kernel_size=[5, 5],
         padding="same",
         activation=tf.nn.relu,
-        kernel_regularizer=regularizer
+        name='CONV1'
     )
 
     # Convolutional Layer #2
     conv2 = tf.layers.conv2d(
         inputs=conv1,
         strides=(2, 2),
-        filters=32,
-        kernel_size=[5, 5],
-        padding="same",
-        activation=tf.nn.relu,
-        kernel_regularizer=regularizer
-    )
-
-    # Convolutional Layer #3
-    conv3 = tf.layers.conv2d(
-        inputs=conv2,
-        strides=(1, 1),
-        filters=64,
-        kernel_size=[3, 3],
-        padding="same",
-        activation=tf.nn.relu,
-        kernel_regularizer=regularizer
-    )
-
-    # Convolutional Layer #4
-    conv4 = tf.layers.conv2d(
-        inputs=conv3,
-        strides=(2, 2),
         filters=64,
         kernel_size=[5, 5],
         padding="same",
         activation=tf.nn.relu,
-        kernel_regularizer=regularizer
+        name='CONV2'
     )
 
     # Dense Layer
-    conv4_flat = tf.reshape(conv4, [-1, 7 * 7 * 64])
+    conv2_flat = tf.reshape(conv2, [-1, 7 * 7 * 64])
     dense = tf.layers.dense(
-        inputs=conv4_flat,
+        inputs=conv2_flat,
         units=1024,
         activation=tf.nn.relu,
-        kernel_regularizer=regularizer
+        name='DENSE'
     )
-    dense = tf.layers.dropout(inputs=dense)
+    dense = tf.layers.dropout(dense, training=mode==tf.estimator.ModeKeys.TRAIN)
 
     # Logits Layer
-    logits = tf.layers.dense(inputs=dense, units=10,
-                             kernel_regularizer=regularizer)
+    logits = tf.layers.dense(
+        inputs=dense, 
+        units=10,
+        name='LOGIT'
+    )
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions={"classes": tf.argmax(input=logits, axis=1)})
@@ -113,8 +98,15 @@ def cnn_model_fn(features, labels, mode, params):
     )
 
     if mode == tf.estimator.ModeKeys.TRAIN:
+
+        # create feature map plot
+        with tf.name_scope("feature_map"):
+            tf.summary.image('conv1', tf.reshape(conv1, [-1, 14, 14, 1]), max_outputs=8)
+            tf.summary.image('conv2', tf.reshape(conv2, [-1, 7, 7, 1]), max_outputs=8)
+        
+        # adam optimizer performs well with drop-out layer
         optimizer = tf.train.AdamOptimizer(
-            learning_rate=0.001,
+            learning_rate=0.001
         )
 
         train_op = optimizer.minimize(
@@ -134,61 +126,73 @@ def cnn_model_fn(features, labels, mode, params):
 
 
 def main(argv):
-    # Load training data
-    data = np.load('data/train.npz')
-
-    # Create feature columns, here there is only one feature (* 28 * 28).
     feature_columns = [
         tf.feature_column.numeric_column(
             key="pixel",
             shape=[28, 28],
-            normalizer_fn=lambda x: tf.image.per_image_standardization(x))]
+            normalizer_fn=lambda x: tf.image.per_image_standardization(x)
+        )
+    ]
 
-    # create classifier
-    classifier = tf.estimator.Estimator(
+    estimator = tf.estimator.Estimator(
         model_fn=cnn_model_fn,
+        model_dir=FLAGS.model_dir,
         params={
             'feature_columns': feature_columns
-        })
-
-    if CROSS_EVAL:
-        train_x, eval_x, train_y, eval_y = train_test_split(
-            data['x'], data['y'].astype(np.int32), test_size=0.2, random_state=0)
-
-        # performs a early stopping for each $NUM_EPOCHS epoches
-        steps_per_train = train_y.size * NUM_EPOCHS // BATCH_SIZE
-        old_eval_result = 0
-        counter = 0
-
-        while(counter < 3):
-            counter += 1
-            classifier.train(
-                input_fn=lambda: train_input_fn(train_x, train_y, BATCH_SIZE),
-                steps=steps_per_train)
-
-            new_eval_result = classifier.evaluate(
-                input_fn=lambda: eval_input_fn(eval_x, eval_y, BATCH_SIZE))['accuracy']
-
-            print("Accuracy: {}".format(new_eval_result))
-            # if there is a improve on the evaluation set, set the counter to 0
-            if new_eval_result - old_eval_result > 1E-5:
-                counter = 0
-            old_eval_result = new_eval_result
-            print("Current counter: {}".format(counter))
-            return
-
-    full_x, full_y = data['x'], data['y'].astype(np.int32)
-    classifier.train(
-        input_fn=lambda: train_input_fn(full_x, full_y, BATCH_SIZE),
-        steps=30000
+        }
     )
-    test_x = np.load('data/test.npz')['x']
-    predictions = classifier.predict(
-        input_fn=lambda: predict_input_fn(test_x, BATCH_SIZE))
-    labels = [pred['classes'] for pred in predictions]
-    df = pd.DataFrame({'ImageId': range(1, len(labels) + 1), 'Label': labels})
-    df.set_index('ImageId', inplace=True)
-    df.to_csv('submission.csv')
+
+    if FLAGS.predict:
+        # load text data and create submission csv
+        test_x = np.load(os.path.join(FLAGS.data_dir, 'test.npz'))['x']
+
+        predictions = estimator.predict(
+            input_fn=lambda: predict_input_fn(test_x, BATCH_SIZE)
+        )
+
+        labels = [pred['classes'] for pred in predictions]
+        df = pd.DataFrame({'ImageId': range(1, len(labels) + 1), 'Label': labels})
+        df.set_index('ImageId', inplace=True)
+        df.to_csv('submission.csv')
+
+    elif FLAGS.heldout:
+        # perform held-out evaluation, check tensorboard for visualization
+        data = np.load(os.path.join(FLAGS.data_dir, 'train.npz'))
+
+        # extract a sample data (with EVAL_BATCH_SIZE) for evalution
+        x, y = data['x'], data['y'].astype(int)
+        eval_ind = np.random.choice(np.arange(y.size), EVAL_BATCH_SIZE, replace=False)
+        train_ind = np.delete(np.arange(y.size), eval_ind)
+        train_x, train_y = x[train_ind], y[train_ind]
+        eval_x, eval_y = x[eval_ind], y[eval_ind]
+
+        train_spec = tf.estimator.TrainSpec(
+            input_fn=lambda: train_input_fn(train_x, train_y, BATCH_SIZE), max_steps=FLAGS.max_iter
+        )
+
+        eval_spec = tf.estimator.EvalSpec(
+            input_fn=lambda: eval_input_fn(eval_x, eval_y, EVAL_BATCH_SIZE),
+            start_delay_secs=60,
+            throttle_secs=60
+        )
+
+        tf.estimator.train_and_evaluate(
+            estimator=estimator,
+            train_spec=train_spec,
+            eval_spec=eval_spec
+        )
+    
+    else:
+        # load entire training set for training (default mode)
+        data = np.load(os.path.join(FLAGS.data_dir, 'train.npz'))
+
+        train_x, train_y = data['x'], data['y'].astype(int)
+
+        estimator.train(
+            input_fn=lambda: train_input_fn(train_x, train_y, BATCH_SIZE), 
+            max_steps=FLAGS.max_iter
+        )
+
 
 
 if __name__ == '__main__':
